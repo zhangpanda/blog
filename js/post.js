8b3f3861d763116d7de780e4b37dcea72a5c28ed
zhangtaifeng <zhangtaifeng@yizhiyao.cn>
2016-03-31 12:52:28 +0800

    去掉code前面的行号

--- js/post.js
+++ js/post.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
         }
     };
 
-    $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
+    $('pre').addClass('prettyprint'); //添加Google code Hight需要的class
 
     $('.entry a').each(function(index,element){
         var href = $(this).attr('href');
